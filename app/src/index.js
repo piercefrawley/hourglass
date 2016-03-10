94cfd76566ec8e0d2894cc31374c8a180b358693
piercefrawley <pierce@procore.com>
2016-03-09 22:49:22 -0800

    Bump React-Router to 2.0.0, use browserHistory object

--- app/src/index.js
+++ app/src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Router } from 'react-router';
 import { render } from 'react-dom';
 import { routes } from './routes';
-import createBrowserHistory from 'history/lib/createBrowserHistory';
+import { browserHistory } from 'react-router';
 
 
 function main() {
@@ -10,7 +10,7 @@ function main() {
 
   document.body.appendChild(app);
 
-  render(<Router routes={routes} history={createBrowserHistory()}/>, app);
+  render(<Router routes={routes} history={browserHistory}/>, app);
 }
 
 main();
