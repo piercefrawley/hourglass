ea649215e9138383f7fe7ac36a041803f1eadf7e
piercefrawley <pierce@procore.com>
2016-03-26 11:47:26 -0700

    Move Routes To Routes File

--- app/src/components/App.jsx
+++ app/src/components/App.jsx
@@ -4,12 +4,9 @@ import { createStore, combineReducers, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
 import { Route, Router, browserHistory } from 'react-router';
 import { syncHistoryWithStore, routerReducer } from 'react-router-redux';
+
 import routes from '../routes';
 import seizure from '../redux/ducks/seizure';
-import Index from './Index';
-import About from './About';
-import Counter from './Counter';
-import Seizure from './Seizure';
 
 const initialState = fromJS({
   seizure: {
@@ -31,12 +28,7 @@ export default class App extends React.Component {
   render() {
     return (
       <Provider store={store}>
-        <Router history={history}>
-          <Route path="/" component={Index}>
-            <Route path="seizure" component={Seizure}/>
-            <Route path="counter" component={Counter}/>
-          </Route>
-        </Router>
+        <Router routes={routes} history={history}/>
       </Provider>
     );
   }
